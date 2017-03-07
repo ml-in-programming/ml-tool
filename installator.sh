02a087a554f5d9fabcea2318ccfd10706d2e0f6d
Sergey-Pravdyukov <sergey.pravdyukov@gmail.com>
2017-03-07 11:38:51 +0300

    Added Python 3.n support

--- installator.sh
+++ installator.sh
@@ -4,4 +4,11 @@ sudo apt-get install python-pip python-dev python-virtualenv
 targetDirectory=$1
 virtualenv --system-site-packages "./$targetDirectory"
 source ./$targetDirectory/bin/activate
-sudo -H pip install --upgrade tensorflow pip
+echo "Choose Python version:"
+echo "1 Python 2.7"
+echo "2 Python 3.n"
+read pythonVersion
+case $pythonVersion in
+  1) sudo -H pip install --upgrade tensorflow pip;;
+  2) sudo -H pip3 install --upgrade tensorflow pip;;
+esac
