eef2c1e0e756d052591f5997a266c6ec054dc984
Sergey-Pravdyukov <sergey.pravdyukov@gmail.com>
2017-03-19 13:20:37 +0300

    Fixed some bugs: added hints, removed sudo, refactored code etc

--- installator.sh
+++ installator.sh
@@ -1,61 +1,87 @@
 #!/bin/bash
-[ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;}
-echo "Determine how to install TensorFlow: "
+
+function readPythonVersion 
+{
+    echo "Choose Python version. If you don't know input 1: "
+    echo "1 Python 2.7"
+    echo "2 Python 3.x"
+    read pythonVersion
+    while [[ $pythonVersion -lt 1 || $pythonVersion -gt 2 ]]
+    do  
+        echo "Select one of the options"
+        read pythonVersion
+    done    
+}
+
+function checkForPip3Existence
+{
+    if ! pip3Loc="$(type -p "pip3")" || [ -z "$pip3Loc" ]; then
+        apt-get -y install python3-pip
+    fi
+}
+
+[ "$UID" -eq 0 ] || { echo -e "This script must be run as root.\nFor example: sudo ./installator.sh"; exit 1;}
+echo "Determine how to install TensorFlow. We recommended virtualenv mechanism: "
 echo "1 virtualenv"
 echo "2 'native' pip"
 echo "3 Docker"
 read mechanismId
+while [[ $mechanismId -lt 1 || $mechanismId -gt 3 ]]
+do  
+    echo "Select one of the options"
+    read mechanismId
+done    
 case $mechanismId in
   1) 
-    sudo apt-get install python-pip python-dev python-virtualenv 
-    targetDirectory=$1
-    virtualenv --system-site-packages "./$targetDirectory"
+    echo "Install TensorFlow with virtualenv mechanism"
+    apt-get -y install python-pip python-dev python-virtualenv 
+    echo "Input target directory that specifies the top of the virtualenv tree. For example './tensorflow'"
+    read targetDirectory
+    virtualenv --system-site-packages $targetDirectory
     source ./$targetDirectory/bin/activate
-    echo "Choose Python version:"
-    echo "1 Python 2.7"
-    echo "2 Python 3.n"
-    read pythonVersion
+    readPythonVersion
     case $pythonVersion in
-      1) sudo -H pip install --upgrade tensorflow pip;;
-      2) sudo -H pip3 install --upgrade tensorflow pip;;
+      1) pip install --upgrade tensorflow pip;;
+      2) 
+        checkForPip3Existence
+        pip3 install --upgrade tensorflow pip
+        ;;
     esac
     ;;
   2)
-    sudo apt-get install python-pip python-dev
-    echo "Choose Python version: "
-    echo "1 Python 2.7"
-    echo "2 Python 3.n"
-    read pythonVersion
+    echo "Install TensorFlow with native pip mechanism"
+    apt-get install python-pip python-dev
+    readPythonVersion
     case $pythonVersion in
-      1)
-        sudo -H pip uninstall tensorflow
-        sudo -H pip install tensorflow 
-        ;;
-      2)
-        sudo -H pip3 uninstall tensorflow
-        sudo -H pip3 install tensorflow
+      1) pip install tensorflow;;
+      2) 
+        checkForPip3Existence
+        pip3 install tensorflow
         ;;
     esac
     ;;
   3)
-    echo "Docker Community Edition for Ubuntu"
-    sudo apt-get -y install apt-transport-https ca-certificates curl
+    echo "Install TensorFlow with Docker Community Edition for Ubuntu mechanism"
+    apt-get -y install apt-transport-https ca-certificates curl
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt-get update
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    apt-get update
 
-    sudo apt-get -y install docker-ce
+    apt-get -y install docker-ce
 
-    sudo docker run hello-world
-
-    echo "Select how you want to use TensorFlow: "
+    echo "Select how you want to use TensorFlow. If you don't know input 1: "
     echo "1 If you plan to run TensorFlow programs from the shell"
     echo "2 If you plan to run TensorFlow programs as Jupyter notebooks"
     echo "3 If you'd like to run TensorBoard inside the container"
     read usingMethod
+    while [[ $usingMethod -gt 4 || $usingMethod -lt 1 ]]
+    do  
+        echo "Select one of the options"
+        read usingMethod
+    done    
     echo "Copy/paste this URL into your browser and choose Tag Name (without 'gpu' substring in the name)"
     echo "https://hub.docker.com/r/tensorflow/tensorflow/tags/"
-    echo "Enter chosen Tag Name:"
+    echo "Input chosen Tag Name, if you don't know input 'latest-py3'"
     read tagName
     case $usingMethod in
       1) docker run -it gcr.io/tensorflow/tensorflow:$tagName bash;;
